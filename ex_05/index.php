af74b55ec9eaf971d3a050d3de47eb6ebc05fe6a
Flavie <flavie.davaine@epitech.eu>
2021-07-07 17:26:53 +0200

    ex_05 ex_08

--- ex_05/index.php
+++ ex_05/index.php
@@ -2,7 +2,7 @@
 $name = NULL;
 if (array_key_exists("name", $_GET)) {
     $name = $_GET["name"];
-    setcookie("name", $name,time()+3600);
+    setcookie("name", $name);
 }
 else if (array_key_exists("name", $_COOKIE)) {
     $name = $_COOKIE["name"];
---
+++ ex_08/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Mon site</title>
+    <head>
+    <body>
+        <?php
+          if (isset($_POST["name"])) {
+                echo "Hello ".$_POST["name"];
+          }
+          else { ?>
+            <form name="form" action="" method="post">
+                <input type="text" name="name" value="">
+                <input type="Submit" value="submit">
+            </form>
+        <?php
+          }
+          ?>
+    </body>
+</html>
